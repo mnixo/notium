8f79d6fedbaaab4ab26bd6098c4d0224ec6c2c37
Bertrand Chauvin <bertrand.chauvin@gmail.com>
2020-09-27 03:08:21 +0200

    Update themes and launcher icon #5

--- lib/themes.dart
+++ lib/themes.dart
@@ -3,20 +3,21 @@ import 'package:flutter/material.dart';
 class Themes {
   static final light = ThemeData(
     brightness: Brightness.light,
-    primaryColor: const Color(0xFF66bb6a),
-    primaryColorLight: const Color(0xFF98ee99),
-    primaryColorDark: const Color(0xFF338a3e),
-    accentColor: const Color(0xff6d4c41),
-    cursorColor: const Color(0xFF338a3e),
-    textSelectionHandleColor: const Color(0xFF66bb6a),
+    primaryColor: const Color.fromRGBO(48, 48, 48, 1),
+    primaryColorLight: const Color.fromRGBO(66, 66, 66, 1),
+    primaryColorDark: const Color.fromRGBO(48, 48, 48, 1),
+    accentColor: const Color.fromRGBO(255, 109, 105, 1),
+    cursorColor: const Color.fromRGBO(0, 0, 0, 1),
+    textSelectionHandleColor: const Color.fromRGBO(0, 0, 0, 1),
+    toggleableActiveColor: Color.fromRGBO(255, 109, 105, 1),
   );
 
   static final dark = ThemeData(
     brightness: Brightness.dark,
-    primaryColor: const Color(0xff212121),
-    accentColor: const Color(0xff689f38),
-    cursorColor: const Color(0xFF66bb6a),
-    textSelectionHandleColor: const Color(0xFF66bb6a),
-    toggleableActiveColor: const Color(0xFF66bb6a),
+    primaryColor: const Color.fromRGBO(48, 48, 48, 1),
+    accentColor: const Color.fromRGBO(205, 109, 105, 1),
+    cursorColor: const Color.fromRGBO(66, 66, 66, 1),
+    textSelectionHandleColor: const Color.fromRGBO(255, 255, 255, 1),
+    toggleableActiveColor: Color.fromRGBO(255, 109, 105, 1),
   );
 }
