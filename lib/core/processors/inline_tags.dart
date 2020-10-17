4b3bf31ef59a303c0c8b20c143e3a97d214b2954
Bertrand Chauvin <bertrand.chauvin@gmail.com>
2020-10-17 22:16:17 +0200

    Use case insensitive tag detection and listing #53

--- lib/core/processors/inline_tags.dart
+++ lib/core/processors/inline_tags.dart
@@ -15,7 +15,7 @@ class InlineTagsProcessor {
         p = '\\+';
       }
 
-      var regexp = RegExp(r"(|\/)\B((" + p + r"(?:(?![×Þß÷þø]))([-'_0-9a-zÀ-ÿ]){1,})+)");
+      var regexp = RegExp(r"(|\/)\B((" + p + r"(?:(?![×Þß÷þø]))([-'_0-9a-zÀ-ÿ]){1,})+)", caseSensitive: false);
       var matches = regexp.allMatches(text);
       for (var match in matches) {
         // Ignore anchors captured at the end of a URL
@@ -29,6 +29,8 @@ class InlineTagsProcessor {
         if (tag.endsWith('.') || tag.endsWith('!') || tag.endsWith('?')) {
           tag = tag.substring(0, tag.length - 1);
         }
+
+        tag = tag.toLowerCase(); // avoids duplicate tags
 
         var all = tag.split(prefix);
         for (var t in all) {
